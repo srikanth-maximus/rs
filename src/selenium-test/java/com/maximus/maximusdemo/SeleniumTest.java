1d5e28460f61346c1c010fd35543845b1a1e6242
Satya Srikanth Pindiproli <satyaspindiproli@maximus.com>
2019-06-20 09:01:33 -0400

    Add a simple selenium test

---
+++ src/selenium-test/java/com/maximus/maximusdemo/SeleniumTest.java
@@ -0,0 +1,42 @@
+package com.maximus.tc.selenium;
+
+import static org.junit.Assert.assertTrue;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class SeleniumTest {
+    
+    WebDriver driver;
+    String baseURL;
+    String hubURL;
+    
+    @Before
+    public void setUp() throws MalformedURLException {
+        baseURL = System.getProperty("testUrl");
+        hubURL = System.getProperty("seleniumUrl");
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setBrowserName("chrome");
+        capabilities.setPlatform(Platform.LINUX);
+        driver = new RemoteWebDriver(new URL(hubURL), capabilities);
+    }
+    
+    @After
+    public void afterTest() {
+        driver.quit();
+    }
+    
+    @Test
+    public void test() {
+        driver.get(baseURL);
+        assertTrue(driver.getPageSource().contains("Hello World - 05/16/2019"));
+    }
+}
